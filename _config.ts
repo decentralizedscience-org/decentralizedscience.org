8618bef364c29d0dd46baecc43bbb8c594c671e6
David E. Perez Negron R. <david@nethunters.xyz>
2024-04-29 15:52:50 -0600

    Adding make relative for ipfs

--- _config.ts
+++ _config.ts
@@ -1,8 +1,13 @@
 import lume from "lume/mod.ts";
 import wiki from "wiki/mod.ts";
+import relativeUrls from "lume/plugins/relative_urls.ts";
 
-const site = lume();
 
+const site = lume({
+  location: new URL("https://dev.decentralizedscience.org"),
+});
+
+site.use(relativeUrls());
 site.use(wiki());
 
 export default site;
